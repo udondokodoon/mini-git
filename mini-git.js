9cd53a981746a379eaa720b31430acaae626750b
udondokodoon <usoda.dondokodoon@gmail.com>
2016-04-04 17:24:12 +0900

    latestBranches 修正

--- mini-git.js
+++ mini-git.js
@@ -1,6 +1,5 @@
 module.exports = (function() {
   var Promise = require("bluebird");
-  var exec = require("child_process").exec;
 
   var MiniGit = function(workingDirPath) {
     this.wd = workingDirPath;
@@ -48,6 +47,7 @@ module.exports = (function() {
    * リモートブランチリスト 最新のN件
    */
   MiniGit.prototype.latestBranches = function(count) {
+    count = count || 10;
     var getCurrentBranch = new Promise(function(resolve, reject) {
       this.git._run(["rev-parse", "--abbrev-ref", "HEAD"], function(err, data) {
         if (err) {
@@ -58,17 +58,17 @@ module.exports = (function() {
     }.bind(this));
     var getBranches = this.fetch().then(function() {
       return new Promise(function(resolve, reject) {
-        exec(["git", "for-each-ref", "--sort='-committerdate'", "--format='%(refname:short) %(subject)'", "refs/remotes", "--count=" + count].join(" "), { cwd: this.wd }, function(err, stdout, stderr) {
+        this.git._run(["for-each-ref", "--sort=-committerdate", "--format=\"%(refname:short) %(subject)\"", "refs/remotes", "--count=" + count], function(err, data) {
           if (err) {
             return reject(err);
           }
-          var list = stdout.split(/\n/).map(function(line) {
+          var list = data.split(/\n/).map(function(line) {
             if (/^\s*$/.test(line)) {
               return null;
             }
             var name, desc;
             var cols = line.split(/\s+/);
-            name = cols.slice(0, 1).join(" ");
+            name = cols.slice(0, 1).join(" ").replace(/"?origin\//, "");
             desc = cols.slice(1).join(" ");
             if (name === "HEAD") {
               return null;
